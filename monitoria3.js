eea22d707f250ecb491812e7af3b4143be6a6760
ISABELLE REIS <isabelle.reis01@fatec.sp.gov.br>
2025-03-31 19:43:26 -0300

    ex03

---
+++ monitoria3.js
@@ -0,0 +1,7 @@
+function MudarCorRodape(){
+    let footer = document.getElementById('footer'); 
+    footer.style.backgroundColor = "green";
+}
+
+//Tarefa 03
+//Site escolhido: https://fatecrl.edu.br/
